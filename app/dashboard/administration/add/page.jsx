11e509415f8655d9bdca2c3aeddfe0396c8c19b6
samiramvo <mvosamira@gmail.com>
2024-04-02 02:27:31 +0100

    ajout

--- app/dashboard/administration/add/page.jsx
+++ app/dashboard/administration/add/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { addUser } from "@/lib/actions";
 import Link from "next/link";
 import "@/styles/globalelements.css"
